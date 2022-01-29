adbf5cb194711707131c630a6d74795243cdbf22
Nisha-2002 <83393857+Nisha-2002@users.noreply.github.com>
2022-01-29 23:58:33 +0530

    Delete hms/admin directory

--- hms/admin/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    if(!isset($_SESSION["name"]))
-    header("Location:authorize.php");
-    else{
-        session_destroy();
-        header("Location:authorize.php");
-    }
-  ?>
